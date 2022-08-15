8087bcbc16b1c1ced1f3a536e8d3be14dc720afd
Miguel_Reis <guelsouzar@gmail.com>
2022-08-15 19:02:33 -0300

    feat: req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,16 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    find = find_news()
+    find.sort(reverse=True, key=lambda x: (x["comments_count"], x["title"]))
+    array = []
+    for news in find:
+        array.append((news["title"], news["url"]))
+    return (array)[:5]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    ""
