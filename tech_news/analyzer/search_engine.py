40ca3ab30b056ed47dbe8ef7f70d1ba2f3a1b564
Miguel_Reis <guelsouzar@gmail.com>
2022-08-15 18:30:41 -0300

    feat: req 7, 8, 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import time
 
 
 # Requisito 6
@@ -11,14 +12,33 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    dateSplit = date.split("-")
+    year = dateSplit[0]
+    mouth = dateSplit[1]
+    day = dateSplit[2]
+    NewDate = f"{day}/{mouth}/{year}"
+    try:
+        time.strptime(date, "%Y-%m-%d")
+        array = []
+        for news in search_news({"timestamp": {"$regex": f"{NewDate}"}}):
+            array.append((news["title"], news["url"]))
+        return array
+    except(ValueError):
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    array = []
+    for news in search_news({"tags": {"$regex": f"{tag}", "$options": "i"}}):
+        array.append((news["title"], news["url"]))
+    return array
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    array = []
+    c = category
+    for news in search_news({"category": {"$regex": f"{c}", "$options": "i"}}):
+        array.append((news["title"], news["url"]))
+    return array
